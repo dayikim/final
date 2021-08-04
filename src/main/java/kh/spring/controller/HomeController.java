fd32c5068502bcbfdec8774bbe698dea7dc9a327
Jun's <pigi-1004@naver.com>
2021-08-04 14:38:50 +0900

    Home create

--- src/main/java/kh/spring/controller/HomeController.java
+++ src/main/java/kh/spring/controller/HomeController.java
@@ -1,5 +1,12 @@
 package kh.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+	
+	@RequestMapping("/")
+	String toHome() {return "Home";}
 
 }
