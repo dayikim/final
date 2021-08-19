4329fb941267edbd444677d0d84c5e3dacca868c
dayikim <wi0910@naver.com>
2021-08-19 14:12:26 +0900

    *

--- src/main/java/kh/spring/service/PointService.java
+++ src/main/java/kh/spring/service/PointService.java
@@ -3,7 +3,11 @@ package kh.spring.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kh.spring.dao.PaymentDAO;
+import kh.spring.dao.PointAccountDAO;
 import kh.spring.dao.PointDAO;
+import kh.spring.dto.PaymentDTO;
+import kh.spring.dto.PointAccountDTO;
 import kh.spring.dto.PointDTO;
 
 
@@ -13,7 +17,13 @@ public class PointService {
 	@Autowired
 	private PointDAO dao;
 	
-
+	@Autowired
+	private PaymentDAO paydao;
+	
+	@Autowired
+	private PointAccountDAO padao;
+	
+	
 	public int charging(PointDTO dto) throws Exception{
 		return dao.charging(dto);
 	}
@@ -33,6 +43,15 @@ public class PointService {
 	public int amount(String sessionID) {
 		
 		return dao.amount(sessionID);
+	}
+
+	public int payment(PaymentDTO dto) {
+		return paydao.payment(dto);
+	}
+
+	public int minus(PointAccountDTO padto) {
+		return padao.minus(padto);
+		
 	}
 	
 
